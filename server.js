0c9bef547a8bc77ee8776c4de5c9f340c32ba088
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-06-21 21:11:32 -0400

    added choices

--- server.js
+++ server.js
@@ -32,6 +32,60 @@ const update = require('./queries/update.js');
  });
 
  const start = () => {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees by Department",
+                "View All Employees by Manager",
+                "Add Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "Remove Employee",
+                "View All Roles",
+                "Add Role",
+                "Remove Role",
+                "View All Departments",
+                "Add Department",
+                "Remove Department",
+                "EXIT"
+            ]
+        })
+        switch (response.choices) {
+            case "View All Employees":
+                await viewAllEmployees();
+                break;
+            case "View All Employees by Department":
+                await viewByDepartment();
+                break;
+            case "View All Employees by Manager":
+                await viewByManager();
+                break;
+            case "Add Data":
+                await addOptions();
+                break;
+            case "Remove Data":
+                await removeOptions();
+                break;
+            case "Update Employee Role":
+                await updateEmployeeRole();
+                break;
+            case "Update Employee Manager":
+                await updateEmployeeManager();
+                break;
+            case "View Department Budget":
+                await getBudget();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+            default:
+                console.log("Not a valid option")
+                break;
+        }
 
  };
 
