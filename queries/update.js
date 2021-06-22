2bd7a8ad7815766696620c572839ddd977a357ed
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-06-22 18:14:37 -0400

    added remove employee

--- queries/update.js
+++ queries/update.js
@@ -124,7 +124,6 @@ function updateManager(connection, cb) {
     })
 };
 
-
 // REMOVE EMPLOYEE
 
 function removeEmployee(connection, cb) {
@@ -156,15 +155,72 @@ function removeEmployee(connection, cb) {
     });
 };
 
-
 // REMOVE ROLE
 
+function removeRole(connection, cb) {
+    connection.query("SELECT * FROM role", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "removeRole",
+                    type: "list",
+                    choices: function () {
+                        let choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].title);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which role would you like to remove?"
+                }
+            ])
+            .then(function (answer) {
+                let query = 'DELETE FROM role WHERE title = ?;'
+                connection.query(query, answer.removeRole, function (err, res) {
+                    if (err) throw err;
+                    console.log("Role successfully deleted");
+                    cb();
+                });
+            });
+    });
+};
+
 // REMOVE DEPARTMENT
+
+function removeDepartment (connection, cb) {
+    connection.query("SELECT * FROM department", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "removeDept",
+                    type: "list",
+                    choices: function () {
+                        let choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which department would you like to remove?"
+                }
+            ])
+            .then(function (answer) {
+                let query = 'DELETE FROM department WHERE name = ?;'
+                connection.query(query, answer.removeDept, function (err, res) {
+                    if (err) throw err;
+                    console.log("Department successfully deleted");
+                    cb();
+                });
+            });
+    });
+}
 
 
 module.exports = {    
     updateRole: updateRole,
     updateManager: updateManager,
     removeEmployee: removeEmployee,
-    
+
 }
