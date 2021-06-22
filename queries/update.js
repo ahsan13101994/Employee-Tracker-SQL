00d93b9f9ad6c0c953f8b29186076d7076ddf4e5
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-06-22 18:11:21 -0400

    added remove employee

--- queries/update.js
+++ queries/update.js
@@ -127,6 +127,36 @@ function updateManager(connection, cb) {
 
 // REMOVE EMPLOYEE
 
+function removeEmployee(connection, cb) {
+    connection.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "removeEmployee",
+                    type: "list",
+                    choices: function () {
+                        let choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].first_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which employee would you like to remove?"
+                }
+            ])
+            .then(function (answer) {
+                let query = 'DELETE FROM employee WHERE first_name = ?;'
+                connection.query(query, answer.removeEmployee, function (err, res) {
+                    if (err) throw err;
+                    console.log("Employee successfully removed");
+                    cb();
+                });
+            });
+    });
+};
+
+
 // REMOVE ROLE
 
 // REMOVE DEPARTMENT
